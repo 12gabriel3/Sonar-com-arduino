a2b0c13b0237f3f11c1f50635ee25a9604083d93
Gabriel Aguiar <aguiargab@gmail.com>
2016-06-17 01:57:26 -0300

    Timer 1 agora trabalha no modo 15 e usa o output capture b

--- projeto-micros/servo.cpp
+++ projeto-micros/servo.cpp
@@ -14,20 +14,20 @@ void Servo::init(){
 	
 	/*
 		Seta o timer1 com as seguintes configuracoes:
-		modo 14: fast-PWM de 16 bits
+		modo 15: fast-PWM de 16 bits
 		Unidade de Output compare canal A ativa
 		Prescaler de 8
 	*/
 	
-	TCCR1A |= 1<<WGM11 | 1<<COM1A1;
+	TCCR1A |= 1<<WGM11 | 1<<WGM10 | 1<<COM1A1;
 	TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS11;
 	
 	//Periodo de 20ms
-	ICR1 = 39999;
+	OCR1A = 39999;
 }
 
 void Servo::setAngle(uint8_t angle){
 	// Faz uma regrinha de tres
 	uint16_t registrador = (uint16_t)angle*(500-300)/18 + 3000;
-	OCR1A = registrador;
+	OCR1B = registrador;
 }
